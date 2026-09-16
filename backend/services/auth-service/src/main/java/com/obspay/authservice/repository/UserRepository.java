78dd7f09cd06d9ba4b81f6c9caa048d21c7af77f
Oba <republicoba1@gmail.com>
2026-09-16 14:21:49 +0100

    feat(auth-service): add UserRepository

---
+++ backend/services/auth-service/src/main/java/com/obspay/authservice/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.obspay.authservice.repository;
+
+import com.obspay.authservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
